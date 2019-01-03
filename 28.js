8a3c8f7a36d3ffcba4c1e5c956fb3b569c3674b2
ftshen <futianshen@MacBook-Pro.local>
2019-01-03 09:05:34 +0800

    [28]

---
+++ 28.js
@@ -0,0 +1,41 @@
+
+console.log(pb('15','456'))
+console.log(pb('123','123'))
+console.log(pb('555','123'))
+console.log(pb('123','456'))
+console.log(pb('123555','456'))
+
+// 解題重點
+// 浮點數誤差 continue
+
+// 190103
+function pb(strM, strN) {
+  if(strM.length>strN.length) return 'Unfair'
+  if(strM.length<strN.length) return 'Fair'
+  if(strM.length===strN.length) {
+    for(i=0; i<strM.length; i++) {
+      if(strM[i]===strN[i]) continue
+      if(strM[i]<strN[i]) return 'Fair'
+      else return 'Unfair'
+    }
+    return 'Unfair'
+  }
+}
+
+//181221 
+function pb(M, N) {
+  M = M.split('')
+  N = N.split('')
+  if (M.length<N.length) return 'Fair'
+  else if (M.length>N.length) return 'Unfair' 
+  else {
+    for (i=0; i<M.length; i++) {
+      if(M[i]===N[i]) continue 
+      else if(M[i]>N[i]) return 'Unfair'
+      else if(M[i]<=N[i]) return 'Fair'
+    }
+    return 'Fair'
+  } 
+}
+
+
